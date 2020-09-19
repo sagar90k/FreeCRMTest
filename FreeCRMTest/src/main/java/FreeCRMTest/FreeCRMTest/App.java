f7aa494b7034ffdd8e6bb9a298e2b30600bb0fb1
AMAR <AMAR@AMAR-PC>
2020-09-19 18:35:32 +0530

    first

---
+++ FreeCRMTest/src/main/java/FreeCRMTest/FreeCRMTest/App.java
@@ -0,0 +1,13 @@
+package FreeCRMTest.FreeCRMTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
